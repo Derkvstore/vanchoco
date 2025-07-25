54bd419bc8a58aad8c7ec24d872209f6166dbc16
Abdel Kader Diarra <abdelkaderdiarra707@gmail.com>
2025-07-25 10:33:01 +0000

    emei identiques

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'; // CORRECTION ICI : '=>' remplacé par 'from'
 import {
   CubeIcon,
   MagnifyingGlassIcon,
